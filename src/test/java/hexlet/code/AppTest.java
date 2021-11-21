f9eb048fa2b01c587377b48711cc744373da8ac1
Saymon <kroxotylik@gmail.com>
2021-11-21 18:22:37 +0300

    add properties

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -1,10 +1,4 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 class AppTest {
-    @Test void appHasAGreeting() {
-        assertNotNull(App.getGreeting(), "app should have a greeting");
-    }
 }
