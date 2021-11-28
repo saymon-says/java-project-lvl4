485aa17b9448e639686f105b1cbff20fb74487e3
Saymon <kroxotylik@gmail.com>
2021-11-28 11:34:57 +0300

    start writing integration tests

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -1,4 +1,51 @@
 package hexlet.code;
 
+import io.ebean.DB;
+import io.ebean.Transaction;
+import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AppTest {
+
+    private static String baseUrl;
+    private static Javalin javalin;
+    private static Transaction transaction;
+
+    @BeforeAll
+    public static void beforeAll() {
+        javalin = App.getApp();
+        javalin.start(0);
+        int port = javalin.port();
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        javalin.stop();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        transaction = DB.beginTransaction();
+    }
+
+    @AfterEach
+    void afterEach() {
+        transaction.rollback();
+    }
+
+    @Test
+    public void testRoot() {
+        HttpResponse<String> response = Unirest.get(baseUrl).asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
 }
