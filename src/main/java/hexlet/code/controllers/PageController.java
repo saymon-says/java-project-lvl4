c5d8b8554d4541d143a38eff5c578a44f6170845
Saymon <kroxotylik@gmail.com>
2021-12-05 11:19:14 +0300

    Handler checks method edit (status 302, Location)

--- src/main/java/hexlet/code/controllers/PageController.java
+++ src/main/java/hexlet/code/controllers/PageController.java
@@ -6,7 +6,6 @@ import hexlet.code.domain.query.QUrl;
 import hexlet.code.domain.query.QUrlCheck;
 import io.ebean.PagedList;
 import io.javalin.http.Handler;
-import io.javalin.http.HttpCode;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.jsoup.Jsoup;
